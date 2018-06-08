ba19a89e9e75e8a7a0e554093a2335410c39182a
taraswww777 <taraswww777@mail.ru>
2018-06-08 13:28:41 +0300

    gulp-imagemin

--- frontend-gulp/gulp.config.js
+++ frontend-gulp/gulp.config.js
@@ -6,19 +6,19 @@ const PATH_DIST = '../www/dist';
 
 
 const config = {
-		SRC: {
-			PATH: PATH_SRC,
-			SASS: PATH_SRC + '/scss/*.{sass,scss}',
-			ASSETS: PATH_SRC + '/assets/**',
-		},
+	SRC: {
+		PATH: PATH_SRC,
+		SASS: PATH_SRC + '/scss/*.{sass,scss}',
+		ASSETS: PATH_SRC + '/assets/**',
+		IMG: PATH_SRC + '/img/**',
+	},
 
-		DIST: {
-			PATH: PATH_DIST,
-			CSS: PATH_DIST + '/css',
-			ASSETS: PATH_DIST + '/assets',
-		}
-
-	}
-;
+	DIST: {
+		PATH: PATH_DIST,
+		CSS: PATH_DIST + '/css',
+		ASSETS: PATH_DIST + '/assets',
+		IMG: PATH_DIST + '/img',
+	},
+};
 
 module.exports = config;
--- frontend-gulp/gulpfile.js
+++ frontend-gulp/gulpfile.js
@@ -3,6 +3,8 @@ const CONFIG = require('./gulp.config');
 
 const gulp = require('gulp');
 const gulpIf = require('gulp-if');
+const newer = require('gulp-newer');
+const imagemin = require('gulp-imagemin');
 const sass = require('gulp-sass');// компиляция sass/scss  в css
 const sourceMaps = require('gulp-sourcemaps');
 const argv = require('yargs').argv;
@@ -28,16 +30,35 @@ gulp.task('clean', () => {
 
 gulp.task('assets', () => {
 	return gulp.src(CONFIG.SRC.ASSETS, {since: gulp.lastRun('assets')})
+		.pipe(newer(CONFIG.DIST.ASSETS))
 		.pipe(gulp.dest(CONFIG.DIST.ASSETS))
 });
 
+gulp.task('images', () => {
+	return gulp.src(CONFIG.SRC.IMG, {since: gulp.lastRun('images')})
+		.pipe(newer(CONFIG.DIST.IMG))
+		.pipe(imagemin([
+			imagemin.gifsicle({interlaced: true}),
+			imagemin.jpegtran({progressive: true}),
+			imagemin.optipng({optimizationLevel: 5}),
+			imagemin.svgo({
+				plugins: [
+					{removeViewBox: true},
+					{cleanupIDs: false}
+				]
+			})
+		]))
+		.pipe(gulp.dest(CONFIG.DIST.IMG))
+});
+
 gulp.task('build', gulp.series('clean',
-	gulp.parallel('sass', 'assets')
+	gulp.parallel('sass', 'assets', 'images')
 ));
 
 gulp.task('watch', () => {
 	gulp.watch(CONFIG.SRC.SASS, gulp.series('sass'));
 	gulp.watch(CONFIG.SRC.ASSETS, gulp.series('assets'));
+	gulp.watch(CONFIG.SRC.IMG, gulp.series('images'));
 });
 
 
