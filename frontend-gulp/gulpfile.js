8e442b440a0f8f6c967b47a26196c078ae428e09
taraswww777 <taraswww777@mail.ru>
2018-06-08 22:57:15 +0300

    server вынесен в отдельный файл

--- frontend-gulp/gulpfile.js
+++ frontend-gulp/gulpfile.js
@@ -4,8 +4,6 @@ const CONFIG = require('./gulp.config');
 
 const gulp = require('gulp');
 
-const browserSync = require('browser-sync').create();
-
 
 function lazyLoadTasks(taskName) {
 	let task = require(CONFIG.gulp_tasks + taskName);
@@ -16,6 +14,7 @@ lazyLoadTasks('sass');
 lazyLoadTasks('clean');
 lazyLoadTasks('assets');
 lazyLoadTasks('images');
+lazyLoadTasks('server');
 
 
 gulp.task('build', gulp.series('clean',
@@ -26,17 +25,6 @@ gulp.task('watch', () => {
 	gulp.watch(CONFIG.SRC.SASS, gulp.series('sass'));
 	gulp.watch(CONFIG.SRC.ASSETS, gulp.series('assets'));
 	gulp.watch(CONFIG.SRC.IMG, gulp.series('images'));
-});
-
-
-gulp.task('server', () => {
-	browserSync.init({
-		server: {
-			baseDir: CONFIG.browserSync.baseDir
-		}
-	});
-
-	browserSync.watch(CONFIG.browserSync.watch).on('change', browserSync.reload);
 });
 
 gulp.task('dev', gulp.series('build', gulp.parallel('watch', 'server')));
