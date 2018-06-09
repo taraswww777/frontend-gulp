fca68a681cac941fb29bb66f73d4ae0b412f1af6
taraswww777 <taraswww777@mail.ru>
2018-06-10 00:41:17 +0300

    дописан минимизатор изображений

--- frontend-gulp/gulpfile.tasks/images.js
+++ frontend-gulp/gulpfile.tasks/images.js
@@ -4,10 +4,27 @@ const CONFIG = require('../gulp.config');
 
 const gulp = require('gulp');
 const _ = require('gulp-load-plugins')();
+const imageminJpegRecompress = require('imagemin-jpeg-recompress');
+const pngquant = require('imagemin-pngquant');
 
 
 module.exports = () => {
 	return gulp.src(CONFIG.SRC.IMG, {since: gulp.lastRun('images')})
 		.pipe(_.newer(CONFIG.DIST.IMG))
+		.pipe(_.cache(_.imagemin([
+			_.imagemin.gifsicle({interlaced: true}),
+			_.imagemin.jpegtran({progressive: true}),
+			imageminJpegRecompress({
+				loops: 5,
+				min: 65,
+				max: 70,
+				quality: 'medium'
+			}),
+			_.imagemin.svgo(),
+			_.imagemin.optipng({optimizationLevel: 3}),
+			pngquant({quality: '65-70', speed: 5})
+		], {
+			verbose: true
+		})))
 		.pipe(gulp.dest(CONFIG.DIST.IMG))
 };
